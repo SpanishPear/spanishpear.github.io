e2329c5a782247d2a8e7946c0f0af9cb1fcd9c6a
Shrey Somaiya <shrey.somaiya@gmail.com>
2022-12-08 18:59:16 +0100

    actually implement the mobile navbar LMAO

--- crates/website/src/components/navbar.rs
+++ crates/website/src/components/navbar.rs
@@ -47,18 +47,26 @@ pub fn navbar() -> Html {
         },
     ];
 
+    let mobile_navbar_showing = use_state(|| false);
+    let mobile_onclick = {
+        let mobile_navbar_showing = mobile_navbar_showing.clone();
+        Callback::from(move |_| {
+            mobile_navbar_showing.set(!*mobile_navbar_showing);
+        })
+    };
+
     html! {
         <nav class="dark:bg-landing-navbar bg-white border-gray-200 px-2 sm:px-4 py-8">
           <div class="container flex flex-wrap justify-between items-center mx-auto">
             <Link<Route> to={Route::Home} classes="flex items-center">
                 <img loading="lazy" src="/assets/logo.svg" class="mr-3 h-9 sm:h-12" width="77" height="67" alt="SpanishPear Icon" />
             </Link<Route>>
-            <button data-collapse-toggle="mobile-menu" class="sm:hidden inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
+            <button data-collapse-toggle="mobile-menu" onclick={mobile_onclick} class="sm:hidden inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
               <span class="sr-only">{"Open Main Menu"}</span>
               <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
               <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
-            <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
+            <div class={classes!("w-full", "md:block", "md:w-auto", if *mobile_navbar_showing {"block"} else {"hidden"})} id="mobile-menu">
               <ul class="flex flex-col items-center mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-base md:font-medium">
                 {
                     nav_items.iter().map(|item| {
